e3b0aa2f12066d9d48185dedbf6d7612b6ea3e0d
Dida Handian <handiandida@gmail.com>
2018-10-23 03:15:49 +0700

    GajiPegawai

---
+++ src/pbo3/pkg10117091/latihan43/gajipegawai/PBO310117091Latihan43GajiPegawai.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan43.gajipegawai;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PBO310117091Latihan43GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+    }
+    
+}
